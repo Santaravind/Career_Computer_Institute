86c21df9da1126738bf495ad0eb2f032d79e7250
Santaravind <aravindsant1310@gmail.com>
2026-06-03 16:02:59 +0530

    Course page changes

--- src/components/courses/Computer.jsx
+++ src/components/courses/Computer.jsx
@@ -1,28 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { BookOpen, Shield, Flame, Hammer, ComputerIcon } from "lucide-react"; // example icons
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-    const courses = [
-  { id: 1, name: "Post Diploma in Computer Hardware Maintenance", duration: "1 Year" },
+const courses = [
+  {
+    id: 1,
+    name: "Post Diploma in Computer Hardware Maintenance",
+    duration: "1 Year",
+  },
   { id: 2, name: "Post Diploma in Computer Applications", duration: "1 Year" },
   { id: 3, name: "Post Diploma in Software Applications", duration: "1 Year" },
   { id: 4, name: "Diploma in Computer Applications", duration: "6 Months" },
-  { id: 5, name: "Diploma in Computer Hardware Maintenance and Network Engineering", duration: "1 Year" },
+  {
+    id: 5,
+    name: "Diploma in Computer Hardware Maintenance and Network Engineering",
+    duration: "1 Year",
+  },
   { id: 6, name: "Diploma in Computer Science", duration: "6 Months" },
   { id: 7, name: "Diploma in Multimedia", duration: "3 Months" },
   { id: 8, name: "Diploma in Web Designing", duration: "6 Months" },
-  { id: 9, name: "Certificate in Mobile Phone Technology", duration: "3 Months" },
+  {
+    id: 9,
+    name: "Certificate in Mobile Phone Technology",
+    duration: "3 Months",
+  },
   { id: 10, name: "Multimedia Animation", duration: "3 Months" },
   { id: 11, name: "Diploma in Data Entry Operator", duration: "3 Months" },
-  { id: 12, name: "Diploma in Computer Hardware and Networking", duration: "6 Months" },
+  {
+    id: 12,
+    name: "Diploma in Computer Hardware and Networking",
+    duration: "6 Months",
+  },
   { id: 13, name: "Certificate in Oracle", duration: "2 Months" },
   { id: 14, name: "Certificate in Visual C++", duration: "2 Months" },
   { id: 15, name: "Certificate in ASP.NET", duration: "4 Months" },
   { id: 16, name: "Certificate in C#.NET", duration: "4 Months" },
   { id: 17, name: "Certificate in VB.NET", duration: "4 Months" },
   { id: 18, name: "Diploma in CAD/CAM Programming", duration: "1 Year" },
-  { id: 19, name: "Diploma in Global Hardware and Networking", duration: "1 Year" },
-  { id: 20, name: "Certificate in Information Technology", duration: "3 Months" },
+  {
+    id: 19,
+    name: "Diploma in Global Hardware and Networking",
+    duration: "1 Year",
+  },
+  {
+    id: 20,
+    name: "Certificate in Information Technology",
+    duration: "3 Months",
+  },
   { id: 21, name: "Preparatory Course in CEO", duration: "4 Months" },
   { id: 22, name: "Certificate in Auto CAD", duration: "2 Months" },
   { id: 23, name: "Preparatory Course in DTP", duration: "4 Months" },
@@ -30,10 +54,26 @@ import { useNavigate } from 'react-router-dom';
   { id: 25, name: "Diploma in Electronic Office", duration: "6 Months" },
   { id: 26, name: "Diploma in Desktop Publishing", duration: "6 Months" },
   { id: 27, name: "Certificate in C++", duration: "2 Months" },
-  { id: 28, name: "Certificate in Clerical Jobs in Computerised Office", duration: "3 Months" },
-  { id: 29, name: "Certificate in Operations of Computer in Retail Shops", duration: "3 Months" },
-  { id: 30, name: "Certificate in Data Entry and Console Operation", duration: "2 Months" },
-  { id: 31, name: "Certificate in Computer Hardware Maintenance", duration: "3 Months" },
+  {
+    id: 28,
+    name: "Certificate in Clerical Jobs in Computerised Office",
+    duration: "3 Months",
+  },
+  {
+    id: 29,
+    name: "Certificate in Operations of Computer in Retail Shops",
+    duration: "3 Months",
+  },
+  {
+    id: 30,
+    name: "Certificate in Data Entry and Console Operation",
+    duration: "2 Months",
+  },
+  {
+    id: 31,
+    name: "Certificate in Computer Hardware Maintenance",
+    duration: "3 Months",
+  },
   { id: 32, name: "C Programming", duration: "2 Months" },
   { id: 33, name: "Visual Basic", duration: "2 Months" },
   { id: 34, name: "MS-Office", duration: "1 Month" },
@@ -48,15 +88,47 @@ import { useNavigate } from 'react-router-dom';
   { id: 43, name: "Internet Applications", duration: "1 Month" },
   { id: 44, name: "Diploma in Computer Faculty Training", duration: "1 Year" },
   { id: 45, name: "Diploma in Computer TC", duration: "10 Months" },
-  { id: 46, name: "Advanced Diploma in Computer Application", duration: "1 Year" },
-  { id: 47, name: "Advanced Diploma in Information Technology", duration: "1 Year" },
-  { id: 48, name: "Honors Diploma in Multimedia Programming", duration: "1 Year" },
-  { id: 49, name: "Advanced Diploma in Multimedia Programming", duration: "6 Months" },
-  { id: 50, name: "Honors Diploma in Computer Application", duration: "6 Months" },
-  { id: 51, name: "Diploma in Laptop Hardware & Wifi Technology", duration: "6 Months" },
+  {
+    id: 46,
+    name: "Advanced Diploma in Computer Application",
+    duration: "1 Year",
+  },
+  {
+    id: 47,
+    name: "Advanced Diploma in Information Technology",
+    duration: "1 Year",
+  },
+  {
+    id: 48,
+    name: "Honors Diploma in Multimedia Programming",
+    duration: "1 Year",
+  },
+  {
+    id: 49,
+    name: "Advanced Diploma in Multimedia Programming",
+    duration: "6 Months",
+  },
+  {
+    id: 50,
+    name: "Honors Diploma in Computer Application",
+    duration: "6 Months",
+  },
+  {
+    id: 51,
+    name: "Diploma in Laptop Hardware & Wifi Technology",
+    duration: "6 Months",
+  },
   { id: 52, name: "Tally ERP 9", duration: "3 Months" },
-  { id: 53, name: "Honors Diploma in Computer Hardware & Networking", duration: "6 Months" },
-  { id: 54, name: "Diploma in Computer Hardware Maintenance", duration: "3 Months" },
+  {
+    id: 53,
+    name: "Honors Diploma in Computer Hardware & Networking",
+    duration: "6 Months",
+  },
+  {
+    id: 54,
+    name: "Diploma in Computer Hardware Maintenance",
+    duration: "3 Months",
+  },
   { id: 55, name: "DOA (MS-Office Only)", duration: "2 Months" },
   { id: 56, name: "Diploma in Graphics", duration: "3 Months" },
   { id: 57, name: "Diploma in 3D Animation", duration: "6 Months" },
@@ -73,44 +145,82 @@ import { useNavigate } from 'react-router-dom';
   { id: 68, name: "Diploma in Information Technology", duration: "1 Year" },
   { id: 69, name: "Diploma in Computer Technology", duration: "2 Years" },
   { id: 70, name: "Diploma in Information Technology", duration: "2 Years" },
-  { id: 71, name: "Certificate in Computer Python Programming", duration: "3 Months" },
+  {
+    id: 71,
+    name: "Certificate in Computer Python Programming",
+    duration: "3 Months",
+  },
   { id: 72, name: "Advanced Diploma in Animation", duration: "1 Year" },
   { id: 73, name: "Diploma in Digital Marketing", duration: "4 Months" },
-  { id: 74, name: "Advanced Diploma in Animation Design and Technology", duration: "1 Year" },
-  { id: 75, name: "Diploma in Visual Design & Communication", duration: "1 Year" },
+  {
+    id: 74,
+    name: "Advanced Diploma in Animation Design and Technology",
+    duration: "1 Year",
+  },
+  {
+    id: 75,
+    name: "Diploma in Visual Design & Communication",
+    duration: "1 Year",
+  },
   { id: 76, name: "Diploma in Game Design", duration: "1 Year" },
   { id: 77, name: "Advanced Diploma in Data Science", duration: "1 Year" },
-  { id: 78, name: "Advanced Diploma in 3D Printing Technology", duration: "1 Year" },
-  { id: 79, name: "Certificate Course in 3D Printing Technology", duration: "6 Months" },
-  { id: 80, name: "Diploma in User Interface and User Experience (UI/UX)", duration: "1 Year" },
+  {
+    id: 78,
+    name: "Advanced Diploma in 3D Printing Technology",
+    duration: "1 Year",
+  },
+  {
+    id: 79,
+    name: "Certificate Course in 3D Printing Technology",
+    duration: "6 Months",
+  },
+  {
+    id: 80,
+    name: "Diploma in User Interface and User Experience (UI/UX)",
+    duration: "1 Year",
+  },
   { id: 81, name: "Diploma in Visual Effects", duration: "1 Year" },
-  { id: 82, name: "Diploma in Graphic Web Designing & Development", duration: "1 Year" },
+  {
+    id: 82,
+    name: "Diploma in Graphic Web Designing & Development",
+    duration: "1 Year",
+  },
 ];
 function Computer() {
-const navigate=useNavigate();
-  const icons = [<BookOpen />, <Shield />, <Flame />, <Hammer />,<ComputerIcon/>];
-   const [showAll, setShowAll] = useState(false);  
+  const navigate = useNavigate();
+  const icons = [
+    <BookOpen />,
+    <Shield />,
+    <Flame />,
+    <Hammer />,
+    <ComputerIcon />,
+  ];
+  const [showAll, setShowAll] = useState(false);
   // Limit to 10 courses initially
   const displayedCourses = showAll ? courses : courses.slice(0, 8);
-  const handalAdmission=(e)=>{
+  const handalAdmission = (e) => {
     e.preventDefault();
-     navigate("/admissions");
-     window.scroll(top);
-  }
+    navigate("/admissions");
+    window.scroll(top);
+  };
 
   return (
     <>
-       <div className="bg-gray-50 py-9 px-6">
-      <h2 className="text-3xl font-bold text-center mb-8 p-3 rounded-full text-black shadow-[5px_5px_5px_5px_rgba(109,40,217)] ">
+      <div className="bg-gray-50 py-9 px-6">
+        {/* <h2 className="text-3xl font-bold text-center mb-8 p-3 rounded-full text-black shadow-[5px_5px_5px_5px_rgba(109,40,217)] ">
         COMPUTER COURSES
-      </h2>
+      </h2> */}
+        <h1 className="text-6xl font-bold text-center mb-2  text-[#0B56A4]  p-3">
+          Our Courses
+        </h1>
+        <div className="w-34 h-2 bg-linear-to-r from-purple-400 to-pink-400 mx-auto rounded-full mb-6 "></div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-7xl mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-7xl mx-auto">
           {displayedCourses.map((course, index) => (
             <div
               key={course.id}
               onClick={handalAdmission}
-              className="flex flex-col items-center cursor-pointer justify-center border border-gray-300 rounded-xl bg-white p-6 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] hover:shadow-[inset_1px_15px_13px_0px_#48bb78] transition"
+              className="flex flex-col items-center cursor-pointer justify-center border border-gray-300 rounded-xl bg-white p-6 shadow-[0_20px_50px_rgba(8,112,184,0.7)] hover:shadow-[inset_1px_15px_13px_0px_#48bb78] transition"
             >
               {/* Icon */}
               <div className="text-yellow-500 mb-3 text-4xl">
@@ -129,20 +239,19 @@ const navigate=useNavigate();
             </div>
           ))}
         </div>
+      </div>
 
-    </div>
-
-    {/* Read More / Show Less Button */}
-        <div className="flex justify-center mt-1">
-          <button
-            onClick={() => setShowAll(!showAll)}
-            className="px-6 py-2 bg-indigo-600 text-white rounded-lg shadow-md hover:bg-indigo-700 transition"
-          >
-            {showAll ? "Show Less" : "Read More"}
-          </button>
-        </div>
+      {/* Read More / Show Less Button */}
+      <div className="flex justify-center mt-1">
+        <button
+          onClick={() => setShowAll(!showAll)}
+          className="px-6 py-2 bg-indigo-600 text-white rounded-lg shadow-md hover:bg-indigo-700 transition"
+        >
+          {showAll ? "Show Less" : "Read More"}
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default Computer
+export default Computer;
